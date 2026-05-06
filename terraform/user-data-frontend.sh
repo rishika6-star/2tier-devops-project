9a62d695c807aec7973784ca3a5065a2c22af5a2
Rishika Sharma <147053821+rishika6-star@users.noreply.github.com>
2026-05-06 20:35:05 +0530

    Update user-data-frontend.sh

--- terraform/user-data-frontend.sh
+++ terraform/user-data-frontend.sh
@@ -1,70 +1,13 @@
-provider "aws" {
-  region = var.region
-}
+#!/bin/bash
 
-# Security Group
-resource "aws_security_group" "devops_sg" {
-  name = "devops-sg"
+apt update -y
+apt install -y docker.io
 
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+systemctl start docker
+systemctl enable docker
 
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+usermod -aG docker ubuntu
 
-  ingress {
-    from_port   = 5000
-    to_port     = 5000
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+docker pull ${docker_username}/frontend-app:latest
 
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
-# Backend Instance
-resource "aws_instance" "backend" {
-  ami           = var.ami
-  instance_type = var.instance_type
-  key_name      = var.key_name
-
-  security_groups = [aws_security_group.devops_sg.name]
-
-  user_data = templatefile("${path.module}/user-data-backend.sh", {
-    docker_username = var.docker_username
-  })
-
-  tags = {
-    Name = "Backend-Server"
-  }
-}
-
-# Frontend Instance
-resource "aws_instance" "frontend" {
-  ami           = var.ami
-  instance_type = var.instance_type
-  key_name      = var.key_name
-
-  security_groups = [aws_security_group.devops_sg.name]
-
-  user_data = templatefile("${path.module}/user-data-frontend.sh", {
-    docker_username = var.docker_username
-  })
-
-  tags = {
-    Name = "Frontend-Server"
-  }
-}
+docker run -d -p 80:80 --name frontend ${docker_username}/frontend-app:latest
